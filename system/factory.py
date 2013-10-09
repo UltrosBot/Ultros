0eb1630282a82c61f29ed65b2e00d060847ea9c0
Gareth Coles <gdude2002@gmail.com>
2013-10-09 14:01:58 +0100

    Reconnection on failure and related config

--- system/factory.py
+++ system/factory.py
@@ -4,7 +4,7 @@ __author__ = "Gareth Coles"
 import importlib
 import logging
 
-from twisted.internet import protocol  # , reactor, ssl
+from twisted.internet import protocol, reactor  # , reactor, ssl
 
 from utils.misc import output_exception
 from utils.log import getLogger
@@ -12,15 +12,25 @@ from utils.log import getLogger
 
 class Factory(protocol.ClientFactory):
     protocol = None
+    reconnecting = False
+    attempts = 0
 
     def __init__(self, protocol_name, config, manager):
         self.logger = getLogger("*" + protocol_name)
         self.config = config
         self.manager = manager
+        manager_config = manager.main_config
+        reconnections = manager_config["reconnections"]
+        self.r_delay = int(reconnections["delay"])
+        self.r_attempts = int(reconnections["attempts"])
+        self.r_on_drop = reconnections["on-drop"]
+        self.r_on_failure = reconnections["on-failure"]
+        self.r_reset = reconnections["reset-on-success"]
 
         try:
             current_protocol = importlib.import_module(
                 "system.protocols.%s.protocol" % protocol_name)
+            self.protocol_class = current_protocol
         except ImportError:
             self.logger.error(
                 "Unable to import protocol %s" % protocol_name)
@@ -30,8 +40,24 @@ class Factory(protocol.ClientFactory):
 
     def clientConnectionLost(self, connector, reason):
         """ Called when the client loses connection """
-        self.logger.warn("Lost connection: %s" % reason)
+        self.logger.warn("Lost connection: %s" % reason.__str__())
+        if self.r_on_drop:
+            self.attempts += 1
+            self.logger.info("Reconnecting after %s seconds (attempt %s/%s)"
+                             % (self.r_delay, self.attempts, self.r_attempts))
+            reactor.callLater(self.r_delay, connector.connect)
+
 
     def clientConnectionFailed(self, connector, reason):
         """ Called when the client fails to connect """
-        self.logger.warn("Connection failed: %s" % reason)
+        self.logger.warn("Connection failed: %s" % reason.__str__())
+        if self.r_on_drop or self.reconnecting:
+            if self.attempts >= self.r_attempts:
+                self.logger.error("Unable to connect after %s attempts, "
+                                  "aborting." % self.attempts)
+                return
+            self.attempts += 1
+            self.logger.info("Reconnecting after %s seconds (attempt %s/%s)"
+                             % (self.r_delay, self.attempts, self.r_attempts))
+            reactor.callLater(self.r_delay, connector.connect)
+
