d4e8b2ace84a56ed3f50428938f8a8708ffcd736
rakiru <rabbitkillrun@googlemail.com>
2013-10-16 16:46:45 +0100

    Generic User/Channel objects for per-protocol ones to inherit from. Certain functions such as msg() should be available on all protocols.

---
+++ system/protocols/generic/channel.py
@@ -0,0 +1,5 @@
+__author__ = 'Sean'
+
+class Channel(object):
+    def __init__(self):
+        self.protocol = None
---
+++ system/protocols/generic/user.py
@@ -0,0 +1,16 @@
+__author__ = 'Sean'
+
+class User(object):
+    def __init__(self):
+        self.protocol = None
+
+    def msg(self, message):
+        raise NotImplementedError("This method must be overridden")
+
+    # TODO: Should these be named some other way to differentiate between
+    # - functions plugins should use and internal use ones like these?
+    def add_channel(self, channel):
+        raise NotImplementedError("This method must be overridden")
+
+    def remove_channel(self, channel):
+        raise NotImplementedError("This method must be overridden")
