abafb7f5da6919237612cfbcdbc19a9dcb212ab7
Gareth Coles <gdude2002@gmail.com>
2013-11-06 12:35:26 +0000

    Small change to the settings.yml.example and added Kitchen to the two output streams.

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
 # coding=utf-8
 
+import sys
+from kitchen.text.converters import getwriter
+
 from utils.log import getLogger, open_log, close_log
 from utils.misc import output_exception
 from system.factory_manager import Manager
+
+sys.stdout = getwriter('utf-8')(sys.stdout)
+sys.stderr = getwriter('utf-8')(sys.stderr)
 
 open_log("output.log")
 
