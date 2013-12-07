1a057154ce5e8d373549a9ddae27f853b6ba1b7b
Gareth Coles <colesgareth2@hotmail.com>
2013-12-07 14:44:08 +0000

    Zombies ate the newline at the end of the file.

--- system/protocols/teamspeak/channel.py
+++ system/protocols/teamspeak/channel.py
@@ -2,13 +2,15 @@
 __author__ = 'Sean'
 
 from system.protocols.generic import channel
+from system.protocols.teamspeak.protocol import Protocol
 
 
 class Channel(channel.Channel):
-    def __init__(self, protocol, name):
+    def __init__(self, protocol, name, cid):
         self.protocol = protocol
         self.name = name
         self.users = set()
+        self.cid = cid
 
     def __str__(self):
         return self.name
@@ -24,3 +26,6 @@ class Channel(channel.Channel):
             self.protocol.log.debug(
                 "Tried to remove non-existent user \"%s\" from channel \"%s\""
                 % (user, self))
+
+    def respond(self, message):
+        self.protocol.send_message(2, self.cid, message)
