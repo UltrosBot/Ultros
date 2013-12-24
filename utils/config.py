5e18c957e9bd6f059ac5cf2ba1b2913c0d645c88
Gareth <gdude2002@gmail.com>
2013-12-24 13:23:28 +0000

    JSONConfig - Why did I write this?

--- utils/config.py
+++ utils/config.py
@@ -1,9 +1,10 @@
 # coding=utf-8
 __author__ = "Gareth Coles"
 
-import yaml
+import json
 import logging
 import os
+import yaml
 
 from utils.misc import output_exception
 from utils.log import getLogger
@@ -27,6 +28,7 @@ class YamlConfig(object):
     For the sake of keeping things sane, all YAML files should end in .yml, but
         this isn't enforced.
     """
+
     data = {}
     exists = True
     fh = None
@@ -49,6 +51,65 @@ class YamlConfig(object):
             return False
         else:
             self.data = yaml.safe_load(self.fh)
+            return True
+
+    def __getitem__(self, y):
+        return self.data.__getitem__(y)
+
+    def __setitem__(self, key, value):
+        raise RuntimeError("Configuration objects are read-only!")
+
+    def __delitem__(self, key):
+        raise RuntimeError("Configuration objects are read-only!")
+
+    def __len__(self):
+        return self.data.__len__()
+
+    def __contains__(self, item):
+        return self.data.__contains__(item)
+
+    def __iter__(self):
+        return self.data.__iter__()
+
+    def __reversed__(self):
+        return self.data.__reversed__()
+
+
+class JSONConfig(object):
+    """
+    Configuration object that uses JSON files for storage.
+
+    This class is similar to the YAML config handler, but it uses JSON.
+
+    Why does this exist when we have YAML?
+    ..heck if I know.
+
+    For sanity's sake, all JSON files should end in .json, but this isn't
+        enforced.
+    """
+
+    data = {}
+    exists = True
+    fh = None
+
+    def __init__(self, filename):
+        self.logger = getLogger("YamlConfig")
+        # Some sanitizing here to make sure people can't escape the config dirs
+        filename = filename.strip("..")
+        self.filename = "config/" + filename
+        self.exists = self.reload()
+
+    def reload(self):
+        if not os.path.exists(self.filename):
+            self.logger.error("File not found: %s" % self.filename)
+            return False
+        try:
+            self.fh = open(self.filename, "r")
+        except Exception:
+            output_exception(self.logger, logging.ERROR)
+            return False
+        else:
+            self.data = json.load(self.fh)
             return True
 
     def __getitem__(self, y):
