81529cceb47e6e16e6d4f4b2a96c9644fc1534eb
Gareth <gdude2002@gmail.com>
2014-03-19 11:59:49 +0000

    Forgot to actually commit the singleton class

---
+++ system/singleton.py
@@ -0,0 +1,28 @@
+__author__ = 'Gareth Coles'
+
+# Found: http://stackoverflow.com/a/6798042/2524563
+
+
+class Singleton(type):
+    """
+    Metaclass for creating classes that will only ever have one instance.
+
+    Use it by doing:
+
+    >>> class Classname(object): __metaclass__ = Singleton
+    >>> x = Classname()
+    >>> y = Classname()
+    >>> x is y
+    True
+
+    Your class will automatically be a singleton, you do not need to call
+    () or any other special function.
+    """
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args,
+                                                                 **kwargs)
+        return cls._instances[cls]
