c25fde95c254086b0022fd62af5e3be826179059
Gareth Coles <gdude2002@gmail.com>
2014-03-29 00:51:26 +0000

    Add notice to setup.py for people who think they know what they're doing.

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
 from distutils.core import setup
 
+print ""
+print "*******************************"
+print "This setup.py is only"
+print "for use with ReadTheDocs."
+print ""
+print "It won't do anything useful"
+print "for you - instead, do this:"
+print ""
+print "pip install -r requirements.txt"
+print "*******************************"
+print ""
+
 setup(
     name='Ultros',
     version='1.0',
