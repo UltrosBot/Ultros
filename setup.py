3257c7370d63b0dbce4a73dd1fcbc83f6c1d2ae4
Gareth Coles <gdude2002@gmail.com>
2014-03-29 00:45:41 +0000

    Setup.py for RTD virtualenv

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='Ultros',
+    version='1.0',
+    url='http://ultos.io',
+    license='',
+    author='Gareth Coles',
+    author_email='ultros@ultros.io',
+    description=''
+)
