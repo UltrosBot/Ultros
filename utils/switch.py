acbb12699fa9fea624fdbb544c89d4f2b90d46a6
Gareth <gdude2002@gmail.com>
2014-05-02 16:14:54 +0100

    A switch, because why not.

---
+++ utils/switch.py
@@ -0,0 +1,27 @@
+__author__ = 'Gareth Coles'
+# Found: http://code.activestate.com/recipes/410692/
+
+
+class Switch(object):
+    def __init__(self, value):
+        self.value = value
+        self.fall = False
+
+    def __iter__(self):
+        """
+        Return the match method once, then stop
+        """
+        yield self.match
+        raise StopIteration
+
+    def match(self, *args):
+        """
+        Indicate whether or not to enter a case suite
+        """
+        if self.fall or not args:
+            return True
+        elif self.value in args:  # changed for v1.5, see below
+            self.fall = True
+            return True
+        else:
+            return False
