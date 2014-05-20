b4966d740fc932e2c71d4600f97a898d5b9ef33c
Gareth <gdude2002@gmail.com>
2014-05-20 11:17:13 +0100

    Little more cleanup. Not sure how to reduce complexity though..

--- doc/conf.py
+++ doc/conf.py
@@ -25,7 +25,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+#  needs_sphinx = '1.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -45,7 +45,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The encoding of source files.
-#source_encoding = 'utf-8-sig'
+#  source_encoding = 'utf-8-sig'
 
 # The master toctree document.
 master_doc = 'index'
