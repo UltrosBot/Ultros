0f7b4916cb6b3464c85496e8b3278c5e9d89636a
Gareth <gdude2002@gmail.com>
2014-06-11 13:04:47 +0100

    Remove unnecessary exception logging from debug plugin.

--- plugins/debug/__init__.py
+++ plugins/debug/__init__.py
@@ -71,20 +71,16 @@ class DebugPlugin(plugin.PluginObject):
             c_obj = code.compile_command(raw_args)
         except SyntaxError as e:
             self.output(__("Syntax error: %s") % e.text)
-            self.logger.exception("Error!")
             return
         except (OverflowError, ValueError) as e:
             self.output(__("Invalid literal: %s") % e.msg)
-            self.logger.exception("Error!")
             return
         except Exception as e:
-            self.logger.exception("Error!")
             self.output("%s" % e)
             return
 
         try:
             self.interpreter.runcode(c_obj)
         except Exception as e:
-            self.logger.exception("Error!")
             self.output("%s" % e)
             return
