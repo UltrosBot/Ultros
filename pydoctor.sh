390cf91fd50c38dea71816df62fc9370df4ff868
Gareth Coles <gdude2002@gmail.com>
2014-08-20 15:22:40 +0100

    Fix package path weirdness

--- pydoctor.sh
+++ pydoctor.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+touch __init__.py
+
 if [ $# -eq 0 ]
 then
     pydoctor --add-package=. --docformat=restructuredtext
 else
     pydoctor --add-package=. --docformat=restructuredtext $@
 fi
+
+rm __init__.py
