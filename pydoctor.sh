a30ff228019d7d78319eeda76c696f97978eb66b
Gareth Coles <gdude2002@gmail.com>
2014-08-20 17:00:09 +0100

    Update pydoctor.sh

--- pydoctor.sh
+++ pydoctor.sh
@@ -4,9 +4,9 @@ touch __init__.py
 
 if [ $# -eq 0 ]
 then
-    pydoctor --add-package=. --docformat=restructuredtext
+    pydoctor --add-package=. --docformat=restructuredtext --project-name=Ultros --project-url=https://ultros.io
 else
-    pydoctor --add-package=. --docformat=restructuredtext $@
+    pydoctor --add-package=. --docformat=restructuredtext --project-name=Ultros --project-url=https://ultros.io $@
 fi
 
 rm __init__.py
