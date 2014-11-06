51474fc889fd752f01c0fccea512d7ad5debf2d3
Gareth Coles <gdude2002@gmail.com>
2014-11-06 15:13:28 +0000

    Line endings, and make the start script check if Ultros is already running.

--- bin/start.sh
+++ bin/start.sh
@@ -17,6 +17,25 @@ SCRIPT_PATH=`pwd`;
 popd  > /dev/null
 cd $SCRIPT_PATH/../
 
+# Check if we're already running..
+
+if [ -f ultros.pid ]; then
+    PID=$(cat ultros.pid)
+
+    if ps -p $PID > /dev/null
+    then
+        echo "Ultros is already running."
+        echo "PID: $(cat ultros.pid)"
+        echo ""
+        echo "Please stop Ultros before trying to run it again."
+        echo ""
+        echo "If you're sure that this is in error, you may delete"
+        echo "'ultros.pid' and try again, but we don't support this."
+
+        exit 1
+    fi
+fi
+
 # OK, now start the process.
 
 echo "Starting process.."
