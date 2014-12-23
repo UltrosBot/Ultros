80c76e8fc9a08791cfb44b7a4ad3abbedc1650d1
Gareth <gdude2002@gmail.com>
2014-12-23 14:43:36 +0000

    Add basic command support to the console, as well as simple help and stop commands

--- system/console.py
+++ system/console.py
@@ -1,8 +1,10 @@
 __author__ = 'Gareth'
 
+import shlex
 import sys
 import time
 
+from system.singleton import Singleton
 
 from twisted.internet import reactor
 from threading import Thread
@@ -18,12 +20,15 @@ if sys.stdout.isatty():
 
 
 class ConsoleMagic(object):
+    __metaclass__ = Singleton
     wrapper = None
     reader = None
     old_stdout = None
     logger = None
 
     wrapped = True
+
+    commands = {}
 
     def __init__(self):
         if not sys.stdout.isatty() or "--no-console" in sys.argv:
@@ -43,12 +48,79 @@ class ConsoleMagic(object):
         sys.stdout = self.wrapper
         sys.stderr = self.wrapper_err
 
+        self.add_command(
+            u"stop", self.command_stop, u"""Syntax: stop
+
+Disconnect and shut down Ultros
+            """
+        )
+
+        self.add_command(
+            u"help", self.command_help, u"""Syntax: help [command]
+
+On its own, lists available commands. Provide a command for more information
+on it.
+            """
+        )
+
+    def add_command(self, command, callback, description=""):
+        command = command.lower()
+
+        if command not in self.commands:
+            self.commands[command] = {
+                u"callback": callback, u"description": description
+            }
+            return True
+        return False
+
+    def remove_command(self, command):
+        command = command.lower()
+
+        if command in self.commands:
+            del self.commands[command]
+            return True
+        return False
+
+    def command_stop(self, args, parsed_args):
+        self.unwrap()
+
+        from system.factory_manager import Manager
+        Manager().unload()
+
+    def command_help(self, args, parsed_args):
+        if parsed_args is not None:
+            args = parsed_args
+
+        if len(args) < 1:
+            self.logger.info(u"Commands: %s" % (
+                u", ".join(sorted(self.commands.keys()))
+            ))
+        else:
+            topic = args[0]
+
+            if topic not in self.commands:
+                self.logger.info(
+                    u"Command not found: %s" % topic
+                )
+                return self.logger.info(
+                    u"Use \"help <command>\" for more information."
+                )
+
+            desc = self.commands[topic].get(
+                u"description", u"No description provided"
+            ).strip().strip(u"\n").strip(u"\r").split(u"\n")
+
+            for line in desc:
+                self.logger.info(u"%s | %s" % (
+                    topic, line
+                ))
+
     def unwrap(self):
         if self.wrapped:
             self.reader.do_stop()
             sys.stdout = self.old_stdout
             sys.stderr = self.old_stderr
-            sys.stdout.write(u"\r%s\r" % (" " * WIDTH))
+            sys.stdout.write(u"\r%s\r" % (u" " * WIDTH))
             self.wrapped = False
 
     def written(self, p_str):
@@ -60,7 +132,7 @@ class ConsoleMagic(object):
         for x in p_str:
             if newlined:
                 newlined = False
-                self.old_stdout.write(u"\r%s\r" % (" " * WIDTH))
+                self.old_stdout.write(u"\r%s\r" % (u" " * WIDTH))
 
             self.old_stdout.write(x)
             if x == "\n":
@@ -79,7 +151,7 @@ class ConsoleMagic(object):
         for x in p_str:
             if newlined:
                 newlined = False
-                self.old_stdout.write(u"\r%s\r" % (" " * WIDTH))
+                self.old_stdout.write(u"\r%s\r" % (u" " * WIDTH))
 
             self.old_stderr.write(x)
             self.old_stderr.flush()
@@ -91,9 +163,33 @@ class ConsoleMagic(object):
         self.old_stdout.flush()
 
     def read(self, buffer):
-        self.old_stdout.write(u"\r\n")
-        self.logger.info(u"Input: %s" % buffer)
+        self.old_stdout.write(u"\n")
+        self.old_stdout.flush()
+        self.logger.debug(u"Input: %s" % buffer)
+
+        if buffer:
+            args = buffer.split()
+
+            if not args:
+                return
+
+            command = args[0].lower()
+            args = u" ".join(args[1:]) if len(args) > 1 else u""
+
+            try:
+                lex = shlex.shlex(args, posix=True)
+                lex.whitespace_split = True
+                lex.quotes = u"\""
+                lex.commenters = u""
+                parsed_args = list(lex)
+            except ValueError:
+                parsed_args = None
+
+            if command in self.commands:
+                return self.commands[command][u"callback"](args, parsed_args)
+
         self.old_stdout.write(u"> ")
+        self.old_stdout.flush()
 
 
 class Wrapper(object):
@@ -179,6 +275,7 @@ class Reader(Thread):
                 self.magic.old_stdout.write(char)
                 self.buffer += char
 
+            self.magic.old_stdout.flush()
             time.sleep(0.001)
 
     def ctrl_c(self):
