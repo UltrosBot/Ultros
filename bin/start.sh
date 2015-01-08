ed66e2252e5b6c81f4a89f27eae91eb4cd431b93
Gareth Coles <gdude2002@gmail.com>
2015-01-08 09:02:57 +0000

    Make daemonizing script optimise aggressively

--- bin/start.sh
+++ bin/start.sh
@@ -41,7 +41,7 @@ fi
 echo "Starting process.."
 echo ""
 
-python run.py --no-console &> logs/ultros.out &
+python -OO run.py --no-console &> logs/ultros.out &
 
 # Remember to wait for the process to start before checking the pid.
 
