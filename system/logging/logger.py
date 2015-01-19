6ea0220004bdd1a09f6955e01477af1693908f10
Gareth Coles <gdude2002@gmail.com>
2015-01-19 12:43:55 +0000

    Documentation, mostly.

--- system/logging/logger.py
+++ system/logging/logger.py
@@ -1,7 +1,12 @@
 __author__ = 'Gareth Coles'
 
 """
+Logbook-based logging, modified a bit to suit our purposes.
 
+Logbook turned out to be fairly complicated. Who knew? Still, it adds a lot
+of things to the logging system that would have been a huge pain to do
+ourselves, and our new logging system allows plugins to modify various aspects
+of logging, as well as provide additional handlers.
 """
 
 import os
@@ -11,20 +16,30 @@ import system.logging.shim as shim
 from logbook import INFO, NOTSET, NullHandler, NTEventLogHandler, SyslogHandler
 from system.logging.handlers.colours import ColourHandler
 
-# ["Ultros", "Application", ERROR, None, None, True]
-
 
 def create_syshandler(*_, **__):
+    """
+    Get yourself the correct system log handler.
+
+    You shouldn't need to use this directly.
+
+    :param _: Unused, ignored
+    :param __: Unused, ignored
+    :return: Logbook logger for logging to this OS's system logs
+    """
+
     if os.name == 'nt':
         return NTEventLogHandler("Ultros")
     return SyslogHandler("Ultros")
 
 
+#: Default handler list
 defaults = [
     "boxcar", "email", "redis", "system", "colour",
     "growl", "libnotify", "twitter", "external", "null"
 ]
 
+#: Storage for the configuration of each handler
 configuration = {
     "format_string": "{record.time:%b %d %Y - %H:%M:%S} | "
                      "{record.channel:<25} | {record.level_name:<8} | "
@@ -48,8 +63,10 @@ configuration = {
     "level": INFO
 }
 
+#: Cached loggers
 loggers = {}
 
+#: Each handler object
 handlers = {
     # Default handlers
 
@@ -67,6 +84,7 @@ handlers = {
     "system": create_syshandler
 }
 
+# The order handlers should be added to loggers
 handler_order = [
     "boxcar", "email", "redis", "colour", "growl",
     "libnotify", "twitter", "external", "system", "null"
@@ -74,6 +92,22 @@ handler_order = [
 
 
 def getLogger(name, add_handlers=True):
+    """
+    Get yourself a logger for normal use.
+
+    This will be the function you want to use for logging, most of the time.
+    You don't have to store the logger, but it's probably more efficient if
+    you do, despite our caching.
+
+    :param name: The name of your logger
+    :param add_handlers: Whether to add all the configured handlers, or skip it
+
+    :type name: basestring
+    :type add_handlers: bool
+
+    :return: A wrapped logger, suitable for use anywhere
+    """
+
     if name in loggers:
         return loggers[name]
     else:
@@ -89,9 +123,12 @@ def getLogger(name, add_handlers=True):
 
 def add_all_handlers(logger):
     """
+    Add all the configured handlers to a logger.
+
+    If you need to use this, you're doing something wrong.
+
     :type logger: shim.OurLogger, shim.LoggerForwarder
-    :param logger:
-    :return:
+    :param logger: The logger to add the handlers to
     """
 
     if not configuration["configured"]:
@@ -142,6 +179,16 @@ def add_all_handlers(logger):
 
 
 def redo_handlers(logger):
+    """
+    Remove all the handlers from a logger and add all the configured handlers
+    to it again.
+
+    This is used on every logger every time the handlers list is modified.
+
+    :param logger: The logger to redo
+    :type logger: basestring, shim.LoggerForwarder
+    """
+
     if isinstance(logger, shim.LoggerForwarder):
         logger_name = logger.name
     else:
@@ -154,11 +201,25 @@ def redo_handlers(logger):
 
 
 def redo_all_handlers():
+    """
+    Remove all handlers from all loggers and add the configured handlers
+    back to them.
+    """
+
     for logger in loggers.itervalues():
         redo_handlers(logger)
 
 
 def configure(config):
+    """
+    For internal use, loads the configuration from a dict.
+
+    This is used internally - you shouldn't need this.
+
+    :param config: Dict to load configuration from
+    :type config: dict
+    """
+
     if config is not None:
         configuration["format_string"] = config.get(
             "format_string", configuration["format_string"]
@@ -183,6 +244,17 @@ def configure(config):
 
 
 def handler_before(this, before):
+    """
+    For handler ordering - specify that your handler should be added before
+        another.
+
+    :param this: Your handler
+    :param before: The handler to put after yours
+
+    :type this: str
+    :type before: str
+    """
+
     if this in handler_order:
         return
 
@@ -193,6 +265,17 @@ def handler_before(this, before):
 
 
 def handler_after(this, after):
+    """
+    For handler ordering - specify that your handler should be added after
+        another.
+
+    :param this: Your handler
+    :param after: The handler to put before yours
+
+    :type this: str
+    :type after: str
+    """
+
     if this in handler_order:
         return
 
@@ -205,9 +288,37 @@ def handler_after(this, after):
 
 
 def add_handler(name, handler):
+    """
+    Register your handler proper. This will recreate all handlers on all
+    loggers, so you should only do this when your plugin is loaded, for
+    instance.
+
+    :param name: The name of your handler
+    :param handler: The handler class
+
+    :type name: str
+    :type handler: object
+    """
+
     if name in handlers:
         return
 
     handlers[name] = handler
 
     redo_all_handlers()
+
+
+def remove_handler(name):
+    """
+    Remove a registered handler. This will recreate all handlers on all
+    loggers, so you should use this very sparingly, in special circumstances
+    only.
+
+    :param name: The name of the handler to remove
+    :type name: str
+    """
+
+    if name in handlers:
+        del handlers[name]
+
+        redo_all_handlers()
