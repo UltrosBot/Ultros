973f3918832785d4c3c4449d07c4e172fe691dc6
Gareth Coles <gdude2002@gmail.com>
2015-07-02 17:08:40 +0100

    Mark matching vars as private

--- plugins/urls/matching.py
+++ plugins/urls/matching.py
@@ -7,7 +7,7 @@ from utils.misc import str_to_regex_flags
 # Want to play with the regex?
 # See https://regex101.com/r/bD7xH7/2
 
-regex = """(?P<prefix>[^\w\s\\n]+|)
+_regex = """(?P<prefix>[^\w\s\\n]+|)
 (?P<protocol>[\w]+)
 :/[/]{1,}
 (?P<basic>[\w]+:[\w]+|)(?:@|)
@@ -15,11 +15,11 @@ regex = """(?P<prefix>[^\w\s\\n]+|)
 (?P<port>:[0-9]+|)
 (?P<url>[^\s\\n]+|)"""
 
-r = re.compile(regex, str_to_regex_flags("iux"))
+_r = re.compile(_regex, str_to_regex_flags("iux"))
 
 
 def extract_urls(text):
-    return re.findall(r, text)
+    return re.findall(_r, text)
 
 
-regex_type = type(r)
+regex_type = type(_r)
