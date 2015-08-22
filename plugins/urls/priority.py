6d1c12d96a10e2b4cc7fe2cd87be15056aa478bf
Gareth Coles <gdude2002@gmail.com>
2015-08-22 21:41:42 +0100

    Enums aren't orderable

--- plugins/urls/priority.py
+++ plugins/urls/priority.py
@@ -3,7 +3,7 @@ from enum import Enum
 __author__ = 'Gareth Coles'
 
 
-class Priority(Enum):
+class Priority(object):
     MONITOR = -100  # Only really for the website handler
     LATEST = 0
     LATER = 20
