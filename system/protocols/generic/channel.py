47bfb2b683782f4e4eebfbdc39a7305721b9f3fd
Gareth Coles <gdude2002@gmail.com>
2015-11-17 09:47:44 +0000

    [#85] Add users set and API to generic channel

--- system/protocols/generic/channel.py
+++ system/protocols/generic/channel.py
@@ -8,7 +8,7 @@ _ = Translations().get()
 class Channel(object):
 
     name = ""
-    users = set()
+    users = None
 
     def __init__(self, name, protocol=None):
         self.name = name
@@ -17,3 +17,15 @@ class Channel(object):
 
     def respond(self, message):
         raise NotImplementedError(_("This method must be overridden"))
+
+    def add_user(self, user):
+        self.users.add(user)
+
+    def remove_user(self, user):
+        try:
+            self.users.remove(user)
+        except KeyError:
+            self.protocol.log.debug(
+                "Tried to remove non-existent user \"%s\" from channel \"%s\""
+                % (user, self)
+            )
