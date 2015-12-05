22a1d9afd7dad897aa8f74a536b3928afe164f32
Gareth Coles <gdude2002@gmail.com>
2015-12-05 13:41:10 +0000

    Deprecate system.plugin.PluginObject

--- system/plugin.py
+++ system/plugin.py
@@ -1,8 +1,14 @@
 # coding=utf-8
 import system.plugins.plugin
+from system.decorators.log import deprecated_class
 
 __author__ = "Gareth Coles"
 
-PluginObject = system.plugins.plugin.PluginObject
 
-__all__ = ("PluginObject", )
+@deprecated_class(
+        hint_message="Use system.plugins.plugin.PluginObject instead"
+)
+class PluginObject(system.plugins.plugin.PluginObject):
+    pass
+
+__all__ = ["PluginObject"]
