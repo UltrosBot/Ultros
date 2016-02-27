7cc7f493aa5ceece26c016eecc6dd39ee3ff6664
Gareth Coles <gdude2002@gmail.com>
2016-02-27 11:42:43 +0000

    Add custom json_encoder module
    This can be used just like the regular JSON module,
    but uses a custom encoder that will check objects for the
    __json__ magic method, which is expected to return a JSON
    string or a dict - something that can be serialized by
    default.

---
+++ system/json_encoder.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+import json
+
+__author__ = 'Gareth Coles'
+
+
+class _Encoder(json.JSONEncoder):
+    def default(self, o):
+        if hasattr(o, "__json__"):
+            return o.__json__()
+        return json.JSONEncoder.default(self, o)
+
+_our_encoder = _Encoder()
+
+
+def dump(obj, fp, skipkeys=False, ensure_ascii=True, check_circular=True,
+         allow_nan=True, cls=None, indent=None, separators=None,
+         encoding='utf-8', default=None, sort_keys=False, **kw):
+    return json.dump(obj, fp, skipkeys, ensure_ascii, check_circular,
+                     allow_nan, _Encoder, indent, separators, encoding,
+                     default, sort_keys, **kw)
+
+
+def dumps(obj, skipkeys=False, ensure_ascii=True, check_circular=True,
+          allow_nan=True, cls=None, indent=None, separators=None,
+          encoding="utf-8", default=None, sort_keys=False, **kw):
+    return json.dumps(obj, skipkeys, ensure_ascii, check_circular,
+                      allow_nan, _Encoder, indent, separators, encoding,
+                      default, sort_keys, **kw)
+
+load = json.load
+loads = json.loads
